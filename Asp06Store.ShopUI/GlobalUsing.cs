da9c198e1f0e1ff7c6a356177e5f8eb16d405cdb
navid.asadipur <navid.asadipur@gmail.com>
2022-09-27 19:20:12 +0330

    add services to Program and add CheckoutViewModel and add viewComponents to Layout

---
+++ Asp06Store.ShopUI/GlobalUsing.cs
@@ -0,0 +1,1 @@
+﻿global using Asp06Store.ShopUI.Extentions;
--- Asp06Store.ShopUI/Program.cs
+++ Asp06Store.ShopUI/Program.cs
@@ -1,4 +1,4 @@
-using asp06Store.ShopUI.Models;
+using Asp06Store.ShopUI.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,18 +8,27 @@ var cnnString = builder.Configuration.GetConnectionString("StoreCnn");
 builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(cnnString));
 
 builder.Services.AddScoped<IProductRepository, EfProductRepository>();
+builder.Services.AddScoped<IOrderRepository, EfOrderRepository>();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<Basket>(c => SessionBasket.GetBasket(c));
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
 app.UseStatusCodePages();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}/page{PageNumber}");
     endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/page{PageNumber}");
+    endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}");
     endpoints.MapDefaultControllerRoute();
 }
 );
