a288d07a0736d05d0618a6feeb6ed0600c1f452f
navid.asadipur <navid.asadipur@gmail.com>
2022-09-27 19:16:04 +0330

    add basket model and SessionBasket and BasketPageViewModel and add BasketController and basket/Index.cshtml

---
+++ Asp06Store.ShopUI/Controllers/BasketController.cs
@@ -0,0 +1,56 @@
+﻿using Asp06Store.ShopUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Asp06Store.ShopUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly Basket sessionBasket;
+
+        public BasketController(IProductRepository productRepository, Basket sessionBasket)
+        {
+            this._productRepository = productRepository;
+            this.sessionBasket = sessionBasket;
+        }
+        public IActionResult Index(string returnUrl)
+        {
+
+            BasketPageViewModel model = new()
+            {
+                Basket = sessionBasket,
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddToBasket(int productId, string returnUrl)
+        {
+            var product = _productRepository.GetById(productId);
+            sessionBasket.Add(product, 1);
+
+            return RedirectToAction(nameof(Index), new { returnUrl = returnUrl});
+        }
+
+        public IActionResult RemoveFromBasket(int productId, string returnUrl)
+        {
+            var product = _productRepository.GetById(productId);
+            sessionBasket.Remove(product);
+
+            return RedirectToAction(nameof(Index), new { returnUrl = returnUrl });
+        }
+
+        //private Basket GetBasket()
+        //{
+        //    return HttpContext.Session.GetJson<Basket>("Basket");
+        //}
+
+        //private void SaveBasket(Basket basket)
+        //{
+        //    HttpContext.Session.setJson("Basket", basket);
+        //}
+    }
+}
