10d9894ace7527c1a1e4b9164277ea75cd20c86e
navid.asadipur <navid.asadipur@gmail.com>
2022-09-27 19:19:17 +0330

    BasketSummary and CategorySideBox components

---
+++ Asp06Store.ShopUI/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Asp06Store.ShopUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp06Store.ShopUI.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+        private readonly Basket basket;
+
+        public BasketSummaryViewComponent(Basket basket)
+        {
+            this.basket = basket;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            
+
+            return View(basket);
+        }
+    }
+}
---
+++ Asp06Store.ShopUI/Components/CategorySideBoxViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Asp06Store.ShopUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp06Store.ShopUI.Components
+{
+    public class CategorySideBoxViewComponent : ViewComponent
+    {
+        private readonly IProductRepository productRepository;
+
+        public CategorySideBoxViewComponent(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var currentCategory = RouteData?.Values["category"];
+            ViewBag.CurrentCategory = currentCategory;
+            return View(productRepository.GetAllCategories());
+        }
+    }
+}
