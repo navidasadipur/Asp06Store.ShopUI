58df85d3e2433a2e65348a78929084ebf8046528
navid.asadipur <navid.asadipur@gmail.com>
2022-10-05 12:30:57 +0330

    add dbcontext and ef repositories to intra.data.sql

---
+++ Asp06Store.Infra.Data.Sql/Common/StoreDbContext.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Asp06Store.ShopUI.Models
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Mobile",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Loptop",
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Pc",
+                }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Name = "sumsumng 1",
+                Description = "sumsumng 1 best mobile for this price",
+                Price = 30_000_000
+            },
+            new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                Name = "sumsumng 2",
+                Description = " sumsumng 2 best mobile for this price",
+                Price = 30_000_000
+            },
+             new Product
+             {
+                 Id = 3,
+                 CategoryId = 1,
+                 Name = "IPhone 3",
+                 Description = " IPhone 2 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 4,
+                 CategoryId = 1,
+                 Name = "IPhone 4",
+                 Description = " IPhone 4 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 5,
+                 CategoryId = 1,
+                 Name = "Xiomi 5",
+                 Description = " IPhone 5 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 6,
+                 CategoryId = 2,
+                 Name = "asus 6",
+                 Description = "sumsumng 1 best mobile for this price",
+                 Price = 30_000_000
+             },
+            new Product
+            {
+                Id = 7,
+                CategoryId = 2,
+                Name = "hp 7",
+                Description = " sumsumng 2 best mobile for this price",
+                Price = 30_000_000
+            },
+             new Product
+             {
+                 Id = 8,
+                 CategoryId = 2,
+                 Name = "vivo 8",
+                 Description = " IPhone 2 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 9,
+                 CategoryId = 2,
+                 Name = "asus 9",
+                 Description = " IPhone 4 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 10,
+                 CategoryId = 2,
+                 Name = "Xiomi 10",
+                 Description = " IPhone 5 best mobile for this price",
+                 Price = 30_000_000
+             },
+            new Product
+            {
+                Id = 11,
+                CategoryId = 3,
+                Name = "pc 11",
+                Description = " sumsumng 2 best mobile for this price",
+                Price = 30_000_000
+            },
+             new Product
+             {
+                 Id = 12,
+                 CategoryId = 3,
+                 Name = "vivo 12",
+                 Description = " IPhone 2 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 13,
+                 CategoryId = 3,
+                 Name = "asus 13",
+                 Description = " IPhone 4 best mobile for this price",
+                 Price = 30_000_000
+             },
+             new Product
+             {
+                 Id = 14,
+                 CategoryId = 3,
+                 Name = "asus 14",
+                 Description = " IPhone 4 best mobile for this price",
+                 Price = 30_000_000
+             }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Asp06Store.Infra.Data.Sql/Products/EfProductRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace Asp06Store.ShopUI.Models
+{
+    public class EfProductRepository : IProductRepository
+    {
+        private readonly StoreDbContext _storeDbContext;
+
+        public EfProductRepository(StoreDbContext storeDbContext)
+        {
+            this._storeDbContext = storeDbContext;
+        }
+        public PagedData<Product> GetAll(int pageNumber, int pageSize, string category)
+        {
+            var result = new PagedData<Product>
+            {
+                PageInfo = new PageInfo
+                {
+                    PageSize = pageSize,
+                    PageNumber = pageNumber
+                }
+            };
+
+            result.Data = _storeDbContext.Products
+                .Where(x => string.IsNullOrWhiteSpace(category) || x.Category.Name == category)
+                .Skip((pageNumber -1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+            result.PageInfo.TotalCount = _storeDbContext.Products
+                .Where(x => string.IsNullOrWhiteSpace(category) || x.Category.Name == category)
+                .Count();
+            return result;
+        }
+
+        public Product GetById(int id)
+        {
+            return _storeDbContext.Products.Find(id);
+        }
+    }
+}
